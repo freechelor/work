93c315748e243037cbbf7051ef6e29cf70bef52a
jason <jasoninus.choi@gmail.com>
2019-12-31 16:23:07 -0600

    mod_12312019

--- Test/src/parallel/thread/LongProsumer.java
+++ Test/src/parallel/thread/LongProsumer.java
@@ -13,32 +13,56 @@ public class LongProsumer implements Runnable{
 	public void run() {
 		Random r = new Random();
 		int cnt = 0;
-		while(true) {
-			if(r.nextInt()%3!=0)
-				worker.addLong(r.nextLong());
-			else
-				worker.removeLong();
-			cnt++;
-			if(cnt>100 && cnt%2==1)
+		System.out.println("worker id ===================================== >" + id);
+		if(id==9) {
+			while(true) {
 				try {
 					worker.printItAll();
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+			}
+		} else {
+			while(true) {
+				if(r.nextInt()%20!=0)
+					worker.addLong(r.nextLong());
+//				else
+//					worker.removeLong();
+				/*cnt++;
+				if(cnt>100 && cnt%2==1)
+					try {
+						worker.printItAll();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}*/
+			}	
 		}
 	}
 	
 	public static void main(String args[]) {
 		Worker w = new Worker();
 		LongProsumer lp = new LongProsumer(w, 0);
-		LongProsumer lp2 = new LongProsumer(w, 9);
+		LongProsumer lp2 = new LongProsumer(w, 1);
+		LongProsumer lp3 = new LongProsumer(w, 2);
+		LongProsumer lp9 = new LongProsumer(w, 9);
 		
 		Thread thread1 = new Thread(lp);
 		Thread thread2 = new Thread(lp2);
+		Thread thread3 = new Thread(lp3);
+		Thread thread9 = new Thread(lp9);
 		
 		thread1.start();
 		thread2.start();
+		thread3.start();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		thread9.start();
 	}
 }
 
